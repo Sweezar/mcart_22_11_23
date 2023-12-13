4ff28e4ddc0cc01bca8b31e2e007d13cad91e079
Garafiev Renat <royal13flush@gmail.com>
2023-12-13 17:49:55 +0300

    task 3 - 8

--- o-servise/kontakty/obratnaya-svyaz.php
+++ o-servise/kontakty/obratnaya-svyaz.php
@@ -1,14 +1,43 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Обратная связь");
-?><?$APPLICATION->IncludeComponent(
-	"bitrix:main.feedback",
-	"",
-	Array(
-		"EMAIL_TO" => "royal13flush@gmail.com",
-		"EVENT_MESSAGE_ID" => array(),
-		"OK_TEXT" => "Спасибо, ваше сообщение принято.",
-		"REQUIRED_FIELDS" => array("NAME","EMAIL","MESSAGE"),
-		"USE_CAPTCHA" => "Y"
-	)
-);?><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
+?>
+<div class="site-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-lg-8 mb-5">
+			<?$APPLICATION->IncludeComponent(
+				"bitrix:main.feedback", 
+				"feedback_form", 
+				array(
+					"EMAIL_TO" => "admin@admin.ru",
+					"EVENT_MESSAGE_ID" => array(
+						0 => "7",
+					),
+					"OK_TEXT" => "Спасибо, ваше сообщение принято.",
+					"REQUIRED_FIELDS" => array(
+						0 => "NAME",
+						1 => "EMAIL",
+						2 => "MESSAGE",
+					),
+					"USE_CAPTCHA" => "Y",
+					"COMPONENT_TEMPLATE" => "feedback_form"
+				),
+				false
+			);?>
+			</div>
+
+			<div class="col-lg-4">
+				<div class="p-4 mb-3 bg-white">
+					<?$APPLICATION->IncludeComponent(
+						"bitrix:main.include",
+						"",
+					Array()
+					);?>
+
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
